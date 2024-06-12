a54922bef3c806793f445b031ee3b7d57e8f40d8
FwexYy <egor.2017.06@gmail.com>
2024-06-12 14:45:58 +0300

    feat: Strict mode usage

---
+++ use_strict/countVowels.js
@@ -0,0 +1,13 @@
+function countVowelsInString(str) {
+  "use strict";
+
+  const foundVowels = str.match(/[aeiou]/gi);
+
+  return foundVowels ? foundVowels.length : null;
+}
+
+const totalVowels = countVowelsInString(
+  "Practice summer 2024. Modsen. JavaScript. React. Redux"
+);
+
+console.log(`Количество гласных: ${totalVowels}`);
