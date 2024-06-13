e11fa9239da0eaf478f46385d7431cbc5fbceb3c
FwexYy <egor.2017.06@gmail.com>
2024-06-13 11:38:27 +0300

    feat: Hoisting

---
+++ variables/hoisting.js
@@ -0,0 +1,7 @@
+console.log(firstVariable); // Undefined
+console.log(secondVariable); // SecondVariable is not defined
+console.log(thirdVariable); // ThirdVariable is not defined
+
+var firstVariable = 1;
+let secondVariable = 2;
+const thirdVariable = 3;
