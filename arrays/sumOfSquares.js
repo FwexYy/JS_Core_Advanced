ef0d3256a4dca70b07e5d261cb698fbff168e60f
FwexYy <egor.2017.06@gmail.com>
2024-06-13 12:36:18 +0300

    docs: Comment added

--- arrays/sumOfSquares.js
+++ arrays/sumOfSquares.js
@@ -2,5 +2,5 @@ function squareSum(num) {
   return num.reduce((sum, value) => sum + value ** 2, 0);
 }
 
-const result = squareSum([1, 2, 3, 4, 5]);
+const result = squareSum([1, 2, 3, 4, 5]); // Сумма квадратов элементов массива равна: 55
 console.log(`Сумма квадратов элементов массива равна: ${result}`);
